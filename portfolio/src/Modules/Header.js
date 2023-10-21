9633909f7eb82bb3eaa7ae8f6d597da2f84a13c6
jackanjel <jackangel.dev@gmail.com>
2023-10-21 09:43:51 +0100

    Header CSS

---
+++ portfolio/src/Modules/Header.js
@@ -0,0 +1,12 @@
+import './CSS/Header.css';
+
+
+function Header (){
+    return (
+        <header className='Header'>
+            <h1>Jack Anjel</h1>
+        </header>
+    );
+}
+
+export default Header;
